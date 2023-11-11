c4fc56af99fd03f32277ac733292bc3ef7f758ea
Khar1tonov-Andrey <ander123345454@mail.ru>
2023-11-11 23:46:12 +0300

    repaired_reqs

--- askPupkin_managements/management/commands/pip.py
+++ askPupkin_managements/management/commands/pip.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
 import pip
 import os
-from askPupkin_managements import djenv
+from askPupkin_managements.djenv import REQUIREMENTS
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
@@ -24,17 +24,16 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
-        djenv.check_env_active()
         is_install = options["install"]
         is_uninstall = options["uninstall"]
         name = options["name"]
         if is_install:
             pip.main(["install", name])
-            os.system(f"pip freeze > {djenv.REQUIREMENTS}")
+            os.system(f"pip freeze > {REQUIREMENTS}")
         elif is_uninstall:
             pip.main(["uninstall", name])
-            os.system(f"pip freeze > {djenv.REQUIREMENTS}")
+            os.system(f"pip freeze > {REQUIREMENTS}")
         else: 
             pip.main(["install", name])
-            os.system(f"pip freeze > {djenv.REQUIREMENTS}")
+            os.system(f"pip freeze > {REQUIREMENTS}")
         
